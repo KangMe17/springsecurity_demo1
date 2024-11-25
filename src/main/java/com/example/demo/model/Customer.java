c0cc4957bcc06e00627a67113a53a95305a448df
KangMe17 <vukhangpln123@gmail.com>
2024-11-25 15:56:40 +0700

    first commit

---
+++ src/main/java/com/example/demo/model/Customer.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Customer {
+	private String id;
+	private String name;
+	private String phoneNum;
+	private String email;
+}
